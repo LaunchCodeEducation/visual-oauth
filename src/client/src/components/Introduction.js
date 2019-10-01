134c3336844824e2c4a6f34a63feb7e18800060f
Vamp <vampiirecodes@gmail.com>
2019-10-01 17:28:59 -0500

    client: Introduction component (rough draft)

---
+++ src/client/src/components/Introduction.js
@@ -0,0 +1,46 @@
+import React from "react";
+
+const Introduction = () => {
+  return (
+    <article>
+      <header>
+        <h2>What is OAuth?</h2>
+      </header>
+
+      <section>
+        <header>
+          <h3>Authentication and Authorization</h3>
+        </header>
+
+        <p>
+          In order to understand any new technology you should begin by
+          understanding why it exists and what problems it aims to solve. We
+          will start by exploring two fundamental concepts used in web
+          development - authentication and authorization. While these two terms
+          are often conflated they each have distinct roles that form the basis
+          of the problem OAuth aims to solve.
+        </p>
+      </section>
+
+      <section>
+        <header>
+          <h3>Authentication Strategies</h3>
+        </header>
+      </section>
+
+      <section>
+        <header>
+          <h3>The Auth Problem</h3>
+        </header>
+      </section>
+
+      <section>
+        <header>
+          <h3>The OAuth Solution</h3>
+        </header>
+      </section>
+    </article>
+  );
+};
+
+export default Introduction;
