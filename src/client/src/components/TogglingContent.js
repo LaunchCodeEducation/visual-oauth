ea9ac11016aae6b50537e76a56d6fd82cb81a758
Vamp <vampiirecodes@gmail.com>
2019-10-02 23:04:59 -0500

    TogglingContent: a container with a trigger for hiding/showing content
    - default uses a button with a label (flips between hide/show)
    - can use a custom trigger with the trigger prop

---
+++ src/client/src/components/TogglingContent.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import { Accordion, Grid, Button } from "semantic-ui-react";
+
+const TogglingContent = props => {
+  const { buttonLabel, trigger, content, children } = props;
+  const [contentVisible, setContentVisibility] = useState(false);
+
+  return (
+    <Grid centered container padded>
+      <Accordion>
+        {trigger ? (
+          <Accordion.Title
+            content={trigger}
+            onClick={() => setContentVisibility(!contentVisible)}
+          />
+        ) : (
+          <Accordion.Title>
+            <Button
+              basic
+              color="blue"
+              onClick={() => setContentVisibility(!contentVisible)}
+              content={`${contentVisible ? "Hide " : "Show "} ${buttonLabel}`}
+            />
+          </Accordion.Title>
+        )}
+        <Accordion.Content
+          active={contentVisible}
+          content={content || children}
+        />
+      </Accordion>
+    </Grid>
+  );
+};
+
+TogglingContent.propTypes = {};
+
+export default TogglingContent;
