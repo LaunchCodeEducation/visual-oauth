1098a1408220491efb58ef7ca7fe319e76c68b3c
Vamp <vampiirecodes@gmail.com>
2019-10-06 02:09:13 -0500

    CodeSnippet: add JSON language type to props enum

--- src/client/src/components/CodeSnippet.js
+++ src/client/src/components/CodeSnippet.js
@@ -5,6 +5,7 @@ import theme from "react-syntax-highlighter/dist/esm/styles/hljs/github-gist";
 
 const CodeSnippet = props => {
   const { language, snippetString } = props;
+
   return (
     <div style={{ textAlign: "left" }}>
       <SyntaxHighlighter style={theme} wrapLines={true} language={language}>
@@ -20,7 +21,7 @@ CodeSnippet.defaultProps = {
 
 CodeSnippet.propTypes = {
   snippetString: PropTypes.string,
-  language: PropTypes.oneOf(["html", "javascript"]).isRequired,
+  language: PropTypes.oneOf(["html", "javascript", "json"]).isRequired,
 };
 
 export default CodeSnippet;
