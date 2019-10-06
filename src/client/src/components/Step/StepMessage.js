f3451c4e7f00203a2d53582ed6d45810982c6e51
Vamp <vampiirecodes@gmail.com>
2019-10-06 02:06:31 -0500

    StepMessage: add StepMessageList and add prop support
    - use shared prop types

--- src/client/src/components/Step/StepMessage.js
+++ src/client/src/components/Step/StepMessage.js
@@ -1,23 +1,45 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Message, Divider } from "semantic-ui-react";
+import { List, Message, Divider } from "semantic-ui-react";
+
+import { nodeOrElementType, nodeOrElementListType } from "../../utils";
+
+export const StepMessageList = props => {
+  const { list, bulleted } = props;
+
+  const listProps = bulleted
+    ? { bulleted: true }
+    : { divided: true, relaxed: "very" };
+
+  return (
+    <List {...listProps} style={{ textAlign: "left" }}>
+      {list.map(content => (
+        <List.Item content={content} />
+      ))}
+    </List>
+  );
+};
+
+StepMessageList.propTypes = {
+  bulleted: PropTypes.bool,
+  list: nodeOrElementListType.isRequired,
+};
 
 const stepMessagePropTypes = {
   header: PropTypes.string,
-  extra: PropTypes.oneOfType([PropTypes.element, PropTypes.node]),
-  list: PropTypes.arrayOf(
-    PropTypes.oneOfType([PropTypes.element, PropTypes.node]),
-  ),
+  list: nodeOrElementListType,
+  bulletedList: PropTypes.bool,
+  extra: nodeOrElementType.isRequired,
 };
 
 export const StepMessage = props => {
-  const { header, list, extra, ...otherProps } = props;
+  const { header, list, bulletedList, extra, ...otherProps } = props;
 
   return (
     <Message {...otherProps}>
       <Message.Header content={header} />
       <Divider />
-      {list && <Message.List items={list} />}
+      {list && <StepMessageList list={list} bulleted={bulletedList} />}
       {extra}
     </Message>
   );
@@ -28,7 +50,13 @@ export const StepDescription = props => (
 );
 
 export const StepInstruction = props => (
-  <StepMessage warning size="large" header="Instructions" {...props} />
+  <StepMessage
+    warning
+    bulletedList
+    size="large"
+    header="Instructions"
+    {...props}
+  />
 );
 
 StepMessage.propTypes = stepMessagePropTypes;
