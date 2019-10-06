99da7156941a0b00c7e01d2604767beaa33793e2
Vamp <vampiirecodes@gmail.com>
2019-10-06 02:11:19 -0500

    StepIcon: refactored to have single source of icon props
    - stepIcons can choose size and fitted prop
    - inlineIcons can set size in single location
    - moved status icon props here as well

--- src/client/src/components/Step/StepIcon.js
+++ src/client/src/components/Step/StepIcon.js
@@ -2,65 +2,117 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Icon } from "semantic-ui-react";
 
-export const stepIconEnum = PropTypes.oneOf(["user", "app", "provider"]);
+export const stepIconEnum = PropTypes.oneOf([
+  "frontend",
+  "backend",
+  "provider",
+]);
+
+const sharedIconProps = {
+  // any shared props go here
+};
 
 export const stepIconProps = {
-  app: {
+  backend: {
+    ...sharedIconProps,
     name: "server",
     color: "blue",
-    label: "App (Server)",
+    label: "Back-end (Server)",
   },
 
-  user: {
+  frontend: {
+    ...sharedIconProps,
     name: "user",
     color: "black",
-    label: "User (Client Site)",
+    label: "Front-end (Browser)",
   },
 
   provider: {
+    ...sharedIconProps,
     name: "server",
     color: "red",
     label: "Provider (Server)",
   },
+
+  status: {
+    null: {
+      ...sharedIconProps,
+      color: "black",
+      name: "long arrow alternate down",
+    },
+
+    true: {
+      ...sharedIconProps,
+      color: "green",
+      name: "check circle",
+    },
+
+    false: {
+      ...sharedIconProps,
+      color: "red",
+      name: "x",
+    },
+  },
 };
 
-export const stepIcons = (size = "massive") => ({
-  app: <Icon {...stepIconProps.app} size={size} />,
-  user: <Icon {...stepIconProps.user} size={size} />,
-  provider: <Icon {...stepIconProps.provider} size={size} />,
+// !! change visual flow icon size here
+export const stepIcons = (size = "small", fitted = false) => ({
+  backend: <Icon {...stepIconProps.backend} size={size} fitted={fitted} />,
+  frontend: <Icon {...stepIconProps.frontend} size={size} fitted={fitted} />,
+  provider: <Icon {...stepIconProps.provider} size={size} fitted={fitted} />,
 });
 
+// !! change inline icon size here
+const inlineIcons = ((size = "small") => {
+  // inline icons have "fitted" prop true (no spaces on either side of icon)
+  const inlineStepIcons = stepIcons(size, true);
+
+  return Object.entries(stepIconProps).reduce((inlineIcons, iconPropsEntry) => {
+    const [iconName, iconProps] = iconPropsEntry;
+
+    // exit early to ignore status icon props (no label)
+    if (!iconProps.label) return inlineIcons;
+
+    // split label to extract short name
+    const [shortName] = iconProps.label.split(" ");
+
+    // get stepIcon at inline size
+    const inlineStepIcon = inlineStepIcons[iconName];
+
+    return {
+      ...inlineIcons,
+      [iconName]: {
+        inline: (
+          <span>
+            {shortName} [{inlineStepIcon}]
+          </span>
+        ),
+        inlineCustom: preIconText => (
+          <span>
+            {preIconText} [{inlineStepIcon}]
+          </span>
+        ),
+      },
+    };
+  }, {});
+})();
+
 export default {
-  user: {
-    icon: size => stepIcons(size).user,
-    props: stepIconProps.user,
-    inline: <span>User [ {stepIcons("").user}]</span>,
-    inlineCustom: preIconText => (
-      <span>
-        {preIconText} [ {stepIcons("").user}]
-      </span>
-    ),
+  frontend: {
+    ...inlineIcons.frontend,
+    icon: stepIcons().frontend,
+    props: stepIconProps.frontend,
   },
 
-  app: {
-    icon: stepIcons.app,
-    props: stepIconProps.app,
-    inline: <span>App [ {stepIcons("").app}]</span>,
-    inlineCustom: preIconText => (
-      <span>
-        {preIconText} [ {stepIcons("").app}]
-      </span>
-    ),
+  backend: {
+    ...inlineIcons.backend,
+    icon: stepIcons().backend,
+    props: stepIconProps.backend,
   },
 
   provider: {
-    icon: stepIcons.provider,
+    ...inlineIcons.provider,
+    icon: stepIcons().provider,
     props: stepIconProps.provider,
-    inline: <span>Provider [ {stepIcons("").provider}]</span>,
-    inlineCustom: preIconText => (
-      <span>
-        {preIconText} [ {stepIcons("").provider}]
-      </span>
-    ),
   },
 };
