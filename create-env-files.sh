9d12b1228cd1572e96b99f6ec9b78fd37ef5a076
Vamp <25523682+the-vampiire@users.noreply.github.com>
2019-10-08 10:41:09 -0500

    fix for .env files instead of .env.test

--- create-env-files.sh
+++ create-env-files.sh
@@ -9,7 +9,7 @@ else
 echo "creating API env file at src/api/.env"
 echo "you will need to paste in your GITHUB_CLIENT_ID and GITHUB_CLIENT_SECRET values"
 
-cat <<- EOF > src/api/.env.test
+cat <<- EOF > src/api/.env
 CLIENT_DOMAIN=http://localhost:3000
 
 GITHUB_CLIENT_ID=
@@ -28,10 +28,10 @@ then
 echo "client env file already exists"
 
 else
-echo "creating client env file at src/api/.env"
-echo "you will need to paste in your GITHUB_CLIENT_ID into the REACT_APP_GITHUB_AUTH_ENDPOINT variable in this file"
+echo "creating client env file at src/client/.env"
+echo "you will need to paste in your client_id into the REACT_APP_GITHUB_AUTH_ENDPOINT variable in this file"
 
-cat <<- EOF > src/client/.env.test
+cat <<- EOF > src/client/.env
 REACT_APP_API_DOMAIN=http://localhost:8008
 
 REACT_APP_GITHUB_AUTH_ENDPOINT=https://github.com/login/oauth/authorize?client_id=REPLACE_WITH_YOUR_GITHUB_CLIENT_ID&redirect_uri=http://localhost:3000/&scope=read:user
