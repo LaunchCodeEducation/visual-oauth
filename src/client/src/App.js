330cac9d4414d1dac4ec82b3ffe7156bc0436c17
Vamp <vampiirecodes@gmail.com>
2020-01-25 22:48:01 -0600

    App: decrease margin and increase font size

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -2,11 +2,11 @@ import React from "react";
 import { Container, Header, Divider } from "semantic-ui-react";
 
 import VisualOAuth from "./steps";
-import Introduction from "./components/Introduction";
+import Introduction from "./components/Introduction/";
 
 function App() {
   return (
-    <Container style={{ margin: "25px" }}>
+    <Container style={{ margin: "25px", fontSize: "1.2rem" }}>
       <Header
         size="huge"
         textAlign="center"
